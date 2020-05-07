5ece185fe633e89ad244aff0c6346e96f370d42b
Joel Witherspoon <joel.witherspoon@gmail.com>
2020-05-07 09:47:24 -0700

    Added views and templates. Use of generic templates

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,13 +7,13 @@ app_name = 'polls'
 urlpatterns = [
 
     # index
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
 
     # detail
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 
     # results
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
 
     # vote
     path('<int:question_id>/vote/', views.vote, name='vote'),
